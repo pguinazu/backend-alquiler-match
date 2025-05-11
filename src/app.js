b6ebcbb96d22c620ad9adc6fd95173e45e30049e
Pablo Guiñazu <Pablo_Guinazu@intranet.itresources>
2025-05-11 03:16:51 -0300

    start empty route

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/properties', propertyRoutes);
 
