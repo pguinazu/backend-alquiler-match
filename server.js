c277d83b3fee7252cc1f0fec0df3564273bfa706
Pablo Guiñazu <Pablo_Guinazu@intranet.itresources>
2025-05-11 00:53:01 -0300

    fix start scripts

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 
 connectDB().then(() => {
     app.listen(PORT, () => {
